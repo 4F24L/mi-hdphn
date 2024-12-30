227e58c9f0c248467efe88a6db30d1573d4eb1be
4F24L <afzalmir2003@gmail.com>
2024-12-30 11:23:46 +0530

    Click fxn added

--- xiaomi.js
+++ xiaomi.js
@@ -24,7 +24,7 @@ black.addEventListener('click', () => {
 const originalText = textMain.innerHTML; // for restore
 
 //dot35
-dot35.addEventListener('mouseover', () => {
+dot35.addEventListener('click', () => {
     colorType.innerHTML = "";
 
     typeImage.src = "./images/earTip.png";
@@ -35,8 +35,103 @@ dot35.addEventListener('mouseover', () => {
 dot35.addEventListener('mouseout', () => {
     setTimeout(() => {
         textMain.innerHTML = originalText; // Restore 
-        div2.innerHTML= "";
+        location.reload();
     }, 2000); 
 });
 
+//dot16
+dot16.addEventListener('click', () => {
+    colorType.innerHTML = "";
+
+    typeImage.src = "./images/transparent.png";
+    typeImage.style.height = "60%";
+    typeImage.style.objectFit = "contain";
+    typeImage.style.transform = "translateY(6%)";
+
+});
+
+dot16.addEventListener('mouseout', () => {
+    setTimeout(() => {
+        textMain.innerHTML = originalText; // Restore 
+        location.reload();
+    }, 2000); 
+});
+
+//dot30
+dot30.addEventListener('click', () => {
+    colorType.innerHTML = "";
+
+    typeImage.src = "./images/noise ANC.png";
+    typeImage.style.width = "45%";
+    // typeImage.style.objectFit = "contain";
+    typeImage.style.transform = "scale(1.1) translateY(10%)";
+
+});
+
+dot30.addEventListener('mouseout', () => {
+    setTimeout(() => {
+        textMain.innerHTML = originalText; // Restore 
+        location.reload();
+    }, 2000); 
+});
+
+//dot40
+dot40.addEventListener('click', () => {
+    colorType.innerHTML = "";
+
+    typeImage.src = "./images/ip54.png";
+    typeImage.style.width = "45%";
+    // typeImage.style.objectFit = "contain";
+    // typeImage.style.transform = "scale(1.25) translateY(10%)";
+
+});
+
+dot40.addEventListener('mouseout', () => {
+    setTimeout(() => {
+        textMain.innerHTML = originalText; // Restore 
+        location.reload();
+    }, 2000); 
+});
+
+//dot8
+dot8.addEventListener('click', () => {
+    colorType.innerHTML = "";
+
+    typeImage.src = "./images/power.png";
+    // typeImage.style.width = "45%";
+    // typeImage.style.objectFit = "contain";
+    typeImage.style.transform = "scale(1.15) translateY(12%)";
+
+});
+
+dot8.addEventListener('mouseout', () => {
+    setTimeout(() => {
+        textMain.innerHTML = originalText; // Restore 
+        location.reload();
+    }, 2000); 
+});
+
+//dot53
+dot53.addEventListener('click', () => {
+    colorType.innerHTML = "";
+
+    typeImage.src = "./images/sensor.png";
+    typeImage.style.width = "45%";
+    // typeImage.style.objectFit = "contain";
+    typeImage.style.transform = "scale(1.15) translateY(12%)";
+
+});
+
+dot53.addEventListener('mouseout', () => {
+    setTimeout(() => {
+        textMain.innerHTML = originalText; // Restore 
+        location.reload();
+    }, 2000); 
+});
+
+
+// Check screen width on page load
+if (window.innerWidth < 800) {
+    alert("Please open this website on a desktop or laptop .");
+}
 
